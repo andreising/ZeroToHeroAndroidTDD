8be3a0ea1b52f50dbfdc00c89879d5f9dbda1539
Андрей Сингелейцев <andreysamarachess@mail.ru>
2025-06-16 21:28:05 +0400

    test9 passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var containTextView = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.removeButton).setOnClickListener {
+            removeView()
+            containTextView = false
+        }
+    }
+
+    private fun removeView() {
+        val container = findViewById<LinearLayout>(R.id.rootLayout)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        container.removeView(textView)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(VISIBILITY_KEY, containTextView)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        containTextView = savedInstanceState.getBoolean(VISIBILITY_KEY)
+        if (!containTextView) removeView()
+    }
+
+    companion object {
+        private const val VISIBILITY_KEY = "visibility_key"
     }
 }
