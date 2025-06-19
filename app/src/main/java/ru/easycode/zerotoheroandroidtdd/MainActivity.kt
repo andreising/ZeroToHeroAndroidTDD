e9039e3a8128120832da4dd90eb519c5e6a73559
Андрей Сингелейцев <andreysamarachess@mail.ru>
2025-06-19 14:46:50 +0400

    test14 passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById<Button>(R.id.actionButton)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        textView = findViewById<TextView>(R.id.titleTextView)
+        button.setOnClickListener {
+            thread {
+                runOnUiThread { onLoad() }
+                Thread.sleep(1000)
+                runOnUiThread { loadDone() }
+            }
+        }
+    }
+
+    private fun onLoad() {
+        button.isEnabled = false
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun loadDone() {
+        button.isEnabled = true
+        progressBar.visibility = View.GONE
+        textView.visibility = View.VISIBLE
     }
 }
